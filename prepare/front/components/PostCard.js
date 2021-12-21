9c303451a642d53b0c97cc4f2e2ab90994e0e640
JunYeong <skyzuni@nate.com>
2021-12-21 12:27:23 +0900

    postForm 구현

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PostCard = () => {
-  return <div></div>;
+  return <div>PostCard</div>;
 };
 
 export default PostCard;
--- prepare/front/components/PostForm.js
+++ prepare/front/components/PostForm.js
@@ -1,15 +1,26 @@
 import { Button, Form, Input } from "antd";
-import { useCallback, useState } from "react";
-import { useSelector } from "react-redux";
+import { useCallback, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addPost } from "../reducers/post";
 
 const PostForm = () => {
-  const onSubmit = useCallback(() => {}, []);
+  const dispatch = useDispatch();
+  const imageInput = useRef();
   const { imagePaths } = useSelector((state) => state.post);
   const [text, setText] = useState("");
 
-  const handleChangeText = (e) => {
+  const onSubmit = useCallback(() => {
+    dispatch(addPost);
+    setText("");
+  }, []);
+
+  const handleChangeText = useCallback((e) => {
     setText(e.target.value);
-  };
+  }, []);
+
+  const onClickImageUpload = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
 
   return (
     <Form
@@ -24,8 +35,8 @@ const PostForm = () => {
         placeholder="어떤 일이 있었나요?"
       ></Input.TextArea>
       <div>
-        <input type="file" multiple hidden></input>
-        <Button>이미지 업로드</Button>
+        <input type="file" multiple hidden ref={imageInput}></input>
+        <Button onClick={onClickImageUpload}>이미지 업로드</Button>
         <Button type="primary" style={{ float: "right" }} htmlType="submit">
           쨱짹
         </Button>
